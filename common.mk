48d6ac229d331a0a1d24b11db550998f882b838a
Tomek CEDRO <cederom@tlen.pl>
2011-06-22 00:47:09 +0200

    SUBMODULE/LIBSWD: Integrated (conditional) libswd build into source tree configure and build process (just as JimTCL) in configure.in and Makefile.am. By default build is enabled, using switch '--disable-internal-libswd' build can be disabled and use external one. Added compilers flags to use internal libswd source tree in common.mk.

--- common.mk
+++ common.mk
@@ -8,3 +8,9 @@ if INTERNAL_JIMTCL
 AM_CPPFLAGS += -I$(top_srcdir)/jimtcl \
 			   -I$(top_builddir)/jimtcl
 endif
+
+if INTERNAL_LIBSWD
+AM_CPPFLAGS += -I$(top_srcdir)/submodules/libswd/src \
+			-I$(top_builddir)/submodules/libswd/src
+endif
+
